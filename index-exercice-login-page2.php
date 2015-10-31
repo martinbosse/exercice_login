a05647efdc2126e9a1c7f29803cd42892b9c943a
vagrant <vagrant@zend.localdomain.local>
2015-10-31 16:45:22 -0400

    ajoute image, css et template de site

---
+++ index-exercice-login-page2.php
@@ -0,0 +1,13 @@
+<?php
+$htmlPage2 = '';
+$htmlPage2 .=  '<div class="mainContainer">';
+$htmlPage2 .= '<div class="section1">';
+$htmlPage2 .=  '<div class="content">';
+$htmlPage2 .=  '<h1>Well there it is:</h1>';
+$htmlPage2 .=   '<p>the one, the only </p>';
+$htmlPage2 .=   '<p class="ultra">page 2</p>';
+$htmlPage2 .=   '<a class=\'roll\' href=\'<?php echo $_SERVER[\'PHP_SELF\'];?>?page=welcome.html\'><span data-title=\'Go to welcome page\'>Go to page 2</span></a>';
+$htmlPage2 .=   '<a class="roll" href="logout.php"><span data-title="logout">logout</span></a>';
+$htmlPage2 .=  '</div>';
+$htmlPage2 .=  '</div>';
+$htmlPage2 .=  '</div>';
---
+++ index-exercice-login-prompt.php
@@ -0,0 +1,12 @@
+<?php
+ 
+$htmlOut = '';
+$htmlOut .= '<div class="mainContainer">';
+$htmlOut .= '<div class="formContainer">';
+$htmlOut .= ' <form action="checklogin.php" method="post">';
+$htmlOut .= '<input type="text" name="user" placeholder="Enter you username">';
+$htmlOut .= '<input type="password" name="password" placeholder="Enter your password"><br>';
+$htmlOut .= '<input type="submit" name="submit" value="submit">';
+$htmlOut .= '</form>';
+$htmlOut .= '</div>';  
+$htmlOut .= '</div>';

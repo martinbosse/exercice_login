a2c15d5263ea0ab1bbd31e3fd136fafd4942d42a
vagrant <vagrant@zend.localdomain.local>
2015-10-31 17:16:55 -0400

    Ajoute style pour les liens

--- index-exercice-login-prompt.php
+++ index-exercice-login-prompt.php
@@ -4,7 +4,7 @@ $htmlOut = '';
 $htmlOut .= '<div class="mainContainer">';
 $htmlOut .= '<div class="formContainer">';
 $htmlOut .= ' <form action="checklogin.php" method="post">';
-$htmlOut .= '<input type="text" name="user" placeholder="Enter you username">';
+$htmlOut .= '<input type="text" name="username" placeholder="Enter you username">';
 $htmlOut .= '<input type="password" name="password" placeholder="Enter your password"><br>';
 $htmlOut .= '<input type="submit" name="submit" value="submit">';
 $htmlOut .= '</form>';
